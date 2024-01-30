115a0af44244518acbecaa35614ed0224decbd6c
phutl-icdvn <phutl@icd-vn.com>
2024-01-30 16:45:21 +0700

    init

---
+++ src/main/java/com/phutl/zillow/ZillowApplication.java
@@ -0,0 +1,13 @@
+package com.phutl.zillow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZillowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZillowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phutl/zillow/ZillowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phutl.zillow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZillowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
